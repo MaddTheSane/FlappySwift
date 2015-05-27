5da5607c3056a20fe316c5ac88b75b1ac67518c4
C.W. Betts <computers57@hotmail.com>
2015-05-27 12:05:26 -0600

    Indent using spaces.

--- FlappyBird/GameViewController.swift
+++ FlappyBird/GameViewController.swift
@@ -10,21 +10,21 @@ import UIKit
 import SpriteKit
 
 extension SKNode {
-	class func unarchiveFromFile(file : String) -> SKNode? {
-		
-		let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks")!
-		
-		if let sceneData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil) {
-			let archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
-			
-			archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-			if let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as? GameScene {
-				archiver.finishDecoding()
-				return scene
-			}
-		}
-		return nil
-	}
+    class func unarchiveFromFile(file : String) -> SKNode? {
+        
+        let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks")!
+        
+        if let sceneData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil) {
+            let archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
+            
+            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+            if let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as? GameScene {
+                archiver.finishDecoding()
+                return scene
+            }
+        }
+        return nil
+    }
 }
 
 class GameViewController: UIViewController {
